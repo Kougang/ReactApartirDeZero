f2b657622216ac4c0ce05719de832ebd7f6a18ef
kougang <kougangsopjio@gmail.com>
2023-08-28 16:13:34 +0200

    formulaire customis

--- app.js
+++ app.js
@@ -244,22 +244,124 @@ Incrementer.defaultProps = {
 
 
 
-function Home ()
-{
+// function Home ()
+// {
 
-    return <div>
-                <Welcome name="roch"/>
-                <Welcome name="alex"/>
-                <Clock/>
-                <Incrementer start={10} />
-                <Incrementer start={100} step={10} />
+//     return <div>
+//                 <Welcome name="roch"/>
+//                 <Welcome name="alex"/>
+//                 <Clock/>
+//                 <Incrementer start={10} />
+//                 <Incrementer start={100} step={10} />
         
+//             </div>
+
+
+// }
+function Field ({name,value,onChange,children} )
+{
+           return(
+            <div className="form-group">
+                <label htmlFor={name}>{children}</label>
+                <input type="text" value={value} onChange={onChange} id={name} name={name} className="form-control"/>
+                  
             </div>
-
-
+        )
 }
-ReactDOM.render(<Home/>, document.querySelector('#app'))
 
 
+function Checkbox ({name,value,onChange,children} )
+{
+           return(
+            <div className="form-check">
+                <label htmlFor={name} className="form-check-label">{children}</label>
+                <input type="checkbox" checked={value} onChange={onChange} id={name} name={name} className="form-check-input"/>
+                  
+            </div>
+        )
+}
+
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            nom: '',
+            prenom: '',
+            newsletter: false
+            
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit(this);
+
+    }
+
+    handleChange(e) {
+        
+            const name = e.target.name
+            const type = e.target.type
+            const value = type==='checked' ? e.target.checked : e.target.value
+
+            this.setState({
+                [name] :e.target.value
+            })
+        
+    }
+    handleSubmit(e)
+    {
+        // e.preventDefault()
+
+        const data = JSON.stringify(this.state)
+        console.log(data)
+
+        this.setState({
+            nom: '',
+            prenom: '',
+            newsletter: false
+
+        })
+
+
+    }
+
+
+
+    render() {
+        return (
+
+            <form className="container" onSubmit={this.handleSubmit}>
+            <Field name='nom' value={this.state.nom} onChange={this.handleChange}> Nom </Field>
+            <Field name='prenom' value={this.state.prenom} onChange={this.handleChange}> Prenom </Field>
+            <Checkbox name="newsletter" value = {this.state.newsletter} onChange={this.handleChange}>s'abboner a la newsletter</Checkbox>
+            
+            <div className="form-group">
+                <button className="btn btn-primary"> envoyer</button>  
+            </div>
+            
+            
+            {JSON.stringify(this.state)}
+
+
+            </form>
+        );
+    }
+}
+
+ReactDOM.render(<Home/>, document.querySelector('#app'));
+
+
+// <div>
+// <label htmlFor="nom">Nom</label>
+// <input type="text" value={this.state.nom} onChange={this.handleChange} id="nom" name="nom"></input>
+// </div>
+
+// <div>
+// <label htmlFor="prenom">Prenom</label>
+// <input type="text" value={this.state.prenom} onChange={this.handleChange} id="prenom" name="prenom"></input>
+// </div>
+
+// <div>
+// <label htmlFor="nom">s'abonner a la newsletter?</label>
+// <input type="checkbox" checked={this.state.newsletter} onChange={this.handleChange} id="newsletter" name="newsletter"></input>
+// </div>
 
 
