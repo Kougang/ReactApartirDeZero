6c5a97da69e833f4ba81d81597d5a7d75613a7a7
kougang <kougangsopjio@gmail.com>
2023-10-15 06:03:51 +0100

    modification du fichier README

--- Ref_video11.js
+++ Ref_video11.js
@@ -11,48 +11,41 @@
 //     }
 // )
 
-class Field extends React.Component
-{
 
-    rend()
-    {
-                return(
+class Field extends React.Component {
+    render() {
+        return (
             <div className="form-group">
-            <input type="text" className="form-control" ref={ref}/>
-
+                <input type="text" className="form-control" ref={this.props.forwardRef} />
             </div>
-        )
+        );
     }
-
-
 }
 
+const FieldWithRef = React.forwardRef((props, ref) => {
+    return <Field forwardRef={ref} />;
+});
+
 class Home extends React.Component {
-    constructor(props)
-    {
-        super(props)
-        this.handleClick = this.handleClick.bind(this),
-        this.input = React.createRef()
-
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+        this.input = React.createRef();
     }
 
-    handleClick(e)
-    {
-        console.log(this.input.current.value)
+    handleClick(e) {
+        console.log(this.input.current.value);
     }
 
-
-  render() {
-
-    return (
-      <div>
-      <Field ref={this.input}/>
-        <button onClick={this.handleClick}>tester</button>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <FieldWithRef ref={this.input} label="demo" />
+                <button onClick={this.handleClick}>tester</button>
+            </div>
+        );
+    }
 }
 
 const root = ReactDOM.createRoot(document.querySelector('#app'));
-root.render(<Home/>);
-
+root.render(<Home />);
